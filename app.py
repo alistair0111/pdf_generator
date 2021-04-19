60934dfaa27ea98655b683bcfa1b4bd22904e06e
Alistair Baretto <alistair.baretto@gmail.com>
2021-04-19 20:11:48 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,186 @@
+from flask import Flask,render_template
+from flask import after_this_request
+from flask import *
+from i2p import i2pconverter, i2pconverterAutoCrop, pdfMerger
+import glob, os
+import io
+import string
+import random
+from multiprocessing import Process
+from datetime import datetime
+import time
+app = Flask(__name__)
+
+
+
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route("/")
+def welcome():
+    return render_template('welcome.html')
+
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/normalPDF')
+def index():
+    return render_template('normal.html')
+
+
+@app.route('/autoCropPDF')
+def autoCropPDF():
+    return render_template('autoCrop.html')
+
+
+@app.route('/mergePDF')
+def mergePDF():
+    return render_template('mergePDF.html')
+    
+# @app.route('/', methods=['POST'])
+# def upload_file():
+#     if request.method == 'POST':
+#         if 'files[]' not in request.files:
+#             return redirect(request.url)
+
+#         files = request.files.getlist('files[]')
+
+#         for file in files:
+#             if file and allowed_file(file.filename):
+#                 filename = secure_filename(file.filename)
+#                 file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+#         flash('File(s) successfully uploaded')
+#         return redirect('/')
+
+@app.route('/converted',methods = ['GET', 'POST'])
+def convert():
+    if request.method == 'POST':
+
+        files = request.files.getlist('img')
+        N = 3
+        folderName = ''.join(random.choices(string.ascii_uppercase +
+                             string.digits, k = N))
+        folderName += "-"
+        now = datetime.now()
+        folderName += now.strftime("%d-%m-%Y--%H-%M-%S")
+
+        global UPLOAD_FOLDER 
+        UPLOAD_FOLDER = os.path.join(folderName)
+
+        if not os.path.isdir(UPLOAD_FOLDER):
+            os.mkdir(UPLOAD_FOLDER)
+        for file in files:
+            if file and allowed_file(file.filename):
+                filename = file.filename
+                file.save(os.path.join(UPLOAD_FOLDER, filename))
+    i2pconverter(files, UPLOAD_FOLDER)
+    return render_template('converted.html')
+
+
+@app.route('/convertAutoCrop',methods = ['GET', 'POST'])
+def convertAutoCrop():
+    if request.method == 'POST':
+
+        files = request.files.getlist('img')
+        N = 3
+        folderName = ''.join(random.choices(string.ascii_uppercase +
+                             string.digits, k = N))
+        folderName += "-"
+        now = datetime.now()
+        folderName += now.strftime("%d-%m-%Y--%H-%M-%S")
+
+        global UPLOAD_FOLDER 
+        UPLOAD_FOLDER = os.path.join(folderName)
+
+        if not os.path.isdir(UPLOAD_FOLDER):
+            os.mkdir(UPLOAD_FOLDER)
+        for file in files:
+            if file and allowed_file(file.filename):
+                filename = file.filename
+                file.save(os.path.join(UPLOAD_FOLDER, filename))
+    i2pconverterAutoCrop(files, UPLOAD_FOLDER)
+    return render_template('converted.html')
+
+
+@app.route('/convertMergePDF',methods = ['GET', 'POST'])
+def convertMergePDF():
+    if request.method == 'POST':
+
+        files = request.files.getlist('img')
+        N = 3
+        folderName = ''.join(random.choices(string.ascii_uppercase +
+                             string.digits, k = N))
+        folderName += "-"
+        now = datetime.now()
+        folderName += now.strftime("%d-%m-%Y--%H-%M-%S")
+
+        global UPLOAD_FOLDER 
+        UPLOAD_FOLDER = os.path.join(folderName)
+
+        if not os.path.isdir(UPLOAD_FOLDER):
+            os.mkdir(UPLOAD_FOLDER)
+        for file in files:
+            if file and allowed_file(file.filename):
+                filename = file.filename
+                file.save(os.path.join(UPLOAD_FOLDER, filename))
+    pdfMerger(files, UPLOAD_FOLDER)
+    return render_template('converted.html')
+
+@app.route('/downloadPDF', methods=['GET'])
+def downloadPDF():
+    filename = UPLOAD_FOLDER+".pdf"
+    try:
+        filelist = [ f for f in os.listdir(UPLOAD_FOLDER) ]
+        for f in filelist:
+            os.remove(os.path.join(UPLOAD_FOLDER, f))
+    except Exception as e:
+        pass
+    try:
+        os.rmdir(UPLOAD_FOLDER)
+    except Exception as e:
+        print(e)
+    print("HOLA ",filename)
+
+    try:
+        with open(filename, 'rb') as fo:
+            return_data = io.BytesIO()
+            return_data.write(fo.read())
+        return_data.seek(0)
+        os.remove(filename)
+    except Exception as e:
+        print(e)
+
+    # print("Above os.remove()")
+    
+    try: 
+        return send_file(return_data, mimetype='application/pdf', as_attachment=True, cache_timeout=0, attachment_filename=filename)
+    except Exception as e:
+        return redirect(url_for('index'))
+
+    # background_remove(filename)
+
+    # return send_file(filename, mimetype='application/pdf', as_attachment=True, cache_timeout=0)
+
+
+def background_remove(path):
+    task = Process(target=rm(path))
+    task.start()
+
+
+def rm(path):
+    os.remove(path)
+
+# def job():
+#     allPDFS = [os.remove(file) for file in os.listdir(os.getcwd()) if file.endswith('.pdf')]
+    
+# schedule.every(5).minutes.do(job)
+
+app.run(debug=True)
+
+# while True:
+#     schedule.run_pending()
+#     time.sleep(1)
---
+++ i2p.py
@@ -0,0 +1,102 @@
+import img2pdf
+import os
+from PIL import Image
+from helperFunction.transform import four_point_transform
+from skimage.filters import threshold_local
+import numpy as np
+import argparse
+import cv2
+import imutils
+from PyPDF2 import PdfFileMerger, PdfFileReader
+
+
+
+def pdfMerger(files, folderName):
+    mergedObject = PdfFileMerger()
+    for pdf in os.listdir(folderName):
+        mergedObject.append(PdfFileReader(open(folderName+"/"+pdf, 'rb')))
+    mergedObject.write(folderName+".pdf")
+
+def i2pconverter(files, folderName):
+
+    for image in os.listdir(folderName):
+        remove_transparency(folderName+"/"+image)
+
+    try:
+        print("i2pcon ",folderName)
+        os.remove(folderName)
+    except Exception as e:
+        pass
+    pdfname = folderName + ".pdf"
+    a4inpt = (img2pdf.mm_to_pt(210),img2pdf.mm_to_pt(297))
+    layout_fun = img2pdf.get_layout_fun(a4inpt)
+    with open(pdfname,'wb') as f:
+        f.write(img2pdf.convert([folderName+"/"+i for i in os.listdir(folderName)], layout_fun=layout_fun))
+
+
+def i2pconverterAutoCrop(files, folderName):
+
+    for image in os.listdir(folderName):
+        remove_transparency(folderName+"/"+image)
+    
+    for image in os.listdir(folderName):
+        autoCropHelper(folderName+"/"+image)
+
+    try:
+        print("i2pcon ",folderName)
+        os.remove(folderName)
+    except Exception as e:
+        pass
+    pdfname = folderName + ".pdf"
+    a4inpt = (img2pdf.mm_to_pt(210),img2pdf.mm_to_pt(297))
+    layout_fun = img2pdf.get_layout_fun(a4inpt)
+    with open(pdfname,'wb') as f:
+        f.write(img2pdf.convert([folderName+"/"+i for i in os.listdir(folderName)], layout_fun=layout_fun))
+    
+
+
+def remove_transparency(image, bg_colour=(255, 255, 255)):
+    im = Image.open(image)
+    
+    # Only process if image has transparency (http://stackoverflow.com/a/1963146)
+    if im.mode in ('RGBA', 'LA') or (im.mode == 'P' and 'transparency' in im.info):
+        newimg = im.convert('RGB')
+        newimg.save(image, 'PNG', quality=80)
+
+    else:
+        pass
+
+
+def autoCropHelper(img):
+    image = cv2.imread(img)
+    ratio = image.shape[0] / 500.0
+    orig = image.copy()
+    image = imutils.resize(image, height = 500)
+
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (5, 5), 0)
+    edged = cv2.Canny(gray, 75, 200)
+
+    cnts = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:5]
+
+    for c in cnts:
+        peri = cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+
+        if len(approx) == 4:
+            screenCnt = approx
+            break
+        
+    try:
+        warped = four_point_transform(orig, screenCnt.reshape(4, 2) * ratio)
+
+        warped = cv2.cvtColor(warped, cv2.COLOR_BGR2GRAY)
+        T = threshold_local(warped, 11, offset = 10, method = "gaussian")
+        warped = (warped > T).astype("uint8") * 255
+
+        cv2.imwrite(img, imutils.resize(warped, height = 650))
+    except Exception as e:
+        print("Error: ", e)
+        cv2.imwrite(img, orig)
