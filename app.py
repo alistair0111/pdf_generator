a60d5002f392f9a08d241ad472476de91ee4ab5d
Alistair Baretto <alistair.baretto@gmail.com>
2021-04-19 23:17:09 +0530

    minor bugs

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ import time
 app = Flask(__name__)
 
 
-UPLOAD_FOLDER = ""
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
